bcf5e9d4fb4d53d55142bdf8133e3361f68abe10
minami.mori <minamimori0604@gmail.com>
2018-06-18 06:57:35 +0000

    task2

--- app/User.php
+++ app/User.php
@@ -9,83 +9,113 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
+   
     protected $hidden = [
         'password', 'remember_token',
     ];
     
-    public function microposts()
-    {
-        return $this->hasMany(Micropost::class);
-    }
-    
-     public function followings()
-    {
-        return $this->belongsToMany(User::class, 'user_follow', 'user_id', 'follow_id')->withTimestamps();
-    }
-
-    public function followers()
-    {
-        return $this->belongsToMany(User::class, 'user_follow', 'follow_id', 'user_id')->withTimestamps();
-    }
-    
-    public function follow($userId)
-    {
-        // confirm if already following
-        $exist = $this->is_following($userId);
-        // confirming that it is not you
-        $its_me = $this->id == $userId;
-    
-        if ($exist || $its_me) {
-            // do nothing if already following
-            return false;
-        } else {
-            // follow if not following
-            $this->followings()->attach($userId);
-            return true;
+        public function microposts()
+        {
+            return $this->hasMany(Micropost::class);
         }
-    }
-
-    public function unfollow($userId)
-    {
-        // confirming if already following
-        $exist = $this->is_following($userId);
-        // confirming that it is not you
-        $its_me = $this->id == $userId;
-    
-    
-        if ($exist && !$its_me) {
-            // stop following if following
-            $this->followings()->detach($userId);
-            return true;
-        } else {
-            // do nothing if not following
-            return false;
+        
+         public function followings()
+        {
+            return $this->belongsToMany(User::class, 'user_follow', 'user_id', 'follow_id')->withTimestamps();
         }
-    }
-
-
-    public function is_following($userId) {
-        return $this->followings()->where('follow_id', $userId)->exists();
-    }
-     public function feed_microposts()
-    {
-        $follow_user_ids = $this->followings()-> pluck('users.id')->toArray();
-        $follow_user_ids[] = $this->id;
-        return Micropost::whereIn('user_id', $follow_user_ids);
-    }    
+    
+        public function followers()
+        {
+            return $this->belongsToMany(User::class, 'user_follow', 'follow_id', 'user_id')->withTimestamps();
+        }
+        
+        public function follow($userId)
+        {
+            $exist = $this->is_following($userId);
+            
+            $its_me = $this->id == $userId;
+        
+            if ($exist || $its_me) {
+                
+                return false;
+            } else {
+                
+                $this->followings()->attach($userId);
+                return true;
+            }
+        }
+    
+        public function unfollow($userId)
+        {
+            
+            $exist = $this->is_following($userId);
+            
+            $its_me = $this->id == $userId;
+        
+        
+            if ($exist && !$its_me) {
+               
+                $this->followings()->detach($userId);
+                return true;
+            } else {
+                
+                return false;
+            }
+        }
+    
+        public function is_following($userId) {
+            return $this->followings()->where('follow_id', $userId)->exists();
+        }
+         public function feed_microposts()
+        {
+            $follow_user_ids = $this->followings()-> pluck('users.id')->toArray();
+            $follow_user_ids[] = $this->id;
+            return Micropost::whereIn('user_id', $follow_user_ids);
+        }    
+        
+        public function favorites()
+        {
+            return $this->belongsToMany(Micropost::class, 'user_favorite', 'user_id', 'favorite_id')->withTimestamps();
+        }
+        
+        
+        public function favorite($postId)
+        {
+        
+            $present = $this->is_favoring($postId);
+            
+            if ($present) {
+                
+                return false;
+            } else {
+               
+                $this->favorites()->attach($postId);
+                return true;
+            }
+        }
+        
+        public function unfavorite($postId)
+        {
+            $present = $this->is_favoring($postId);
+        
+            if ($present) {
+               
+                $this->favorites()->detach($postId);
+                return true;
+            } else {
+                
+                return false;
+            }
+        }
+        
+        
+        public function is_favoring($postId) {
+            return $this->favorites()->where('favorite_id', $postId)->exists();
+        }
     
 }
